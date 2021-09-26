229f8f61f0e8c8274d1dc1c823edac87cefbc14e
firthy1234 <chrisfirth_999@hotmail.com>
2021-09-26 23:18:35 +0100

    Game complete

--- hangman.py
+++ hangman.py
@@ -1,24 +1,19 @@
-# rules:
-# - The program must run in Python3 without error.
-# - The script must ONLY stop to either ask for the next guess or because the game has been won or lost (i.e. no menus or other user input).
-# - Asking the user to make her next guess must use the following text (case sensitive and a space after the colon are necessary (I think)): “Please enter your next guess: “
-# - The text printed before “Please enter your next guess: “ must END in the word to be guessed with the unknown letters starred out (i.e. hello would start as ***** and change into *e*** after ‘e’ was guessed). The string must not contain any other stars.
-# - The program must print either “congratulations you win” or “you lose” on exit (not case sensitive).
-# - When a game is played, a word must be picked randomly (from a uniform distribution) from the word_list.txt file.
-# - The word_list.txt file must be stored in the same path as your code and when you load the file don't include the path (i.e. "open('word_list.txt', ...)").
-# - If the user makes 7 wrong guesses then they lose the game.
-
 # A simple game of hangman
 # Created by Chris Firth on 26 Sep 21
 
-# import file here ###############################
-# ....
+#import the uniform library
+import random
 
-# pick random rowd here ###########################
-# ...
+# open the word list and send the contents to a list called words
+f = open("word_list.txt","r")
+words = f.readlines()
 
-# temporary word:
-word = "hello"
+# pick a random word from the words list
+random_number = int(random.uniform(0.0, 99.0))
+word = (words[random_number])
+
+# remove the \n from the word
+word = word[:-1]
 
 # assign a blank string to the hashed_word variable
 hashed_word = ""
@@ -59,6 +54,7 @@ for x in range(7):
     
     #if the user fails to guess all the letters after their 7th attempt
     if x == 6:
+        print(word)
         print("you lose")
         break
 
